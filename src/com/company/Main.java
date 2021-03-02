646dc48ac69202a671d8403481f8c40ca0dd4263
Lanik <Lanik142@gmail.com>
2021-03-03 05:36:32 +0700

    Лого + название хыых

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,26 @@ package com.company;
 
 //import static com.company.ControlFrame.createGUI;
 
+import com.company.ant.WorkerAnt;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.io.File;
+import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args) {
-        AntExample window = new AntExample("Hello ant");
+        AntExample window = new AntExample("ЪУЪ");
         window.setVisible(true);
+        Image icon = null;
+        try {
+            icon = ImageIO.read(new File("src/com/company/Picture/logo.png"));
+        } catch (IOException e) { System.out.println("нет"); }
+        window.setIconImage(icon);
         window.setSize(1000,1000);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.run();
