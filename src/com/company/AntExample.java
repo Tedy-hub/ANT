b77298d0acb4fdbb6be9c870061d9435efdad50a
Alex <alexydenkov99@gmail.com>
2021-04-05 23:30:57 +0700

    ANTP-5 Тестовый проект тестовая правка

--- src/com/company/AntExample.java
+++ src/com/company/AntExample.java
@@ -298,6 +298,7 @@ public class AntExample extends JFrame {
 
     public void configureMenu(){
         MyMenu.simOptionStart.addActionListener(this::actionStart);
+
         MyMenu.simOptionStop.addActionListener(this::actionStop);
 
         MyMenu.infoOptionShow.addActionListener(new ActionListener() {
