62941f2894dc52bcb40394cc7581c4e478f17c3a
Alex <alexydenkov99@gmail.com>
2021-04-16 11:44:00 +0700

    #ANTP-1 добавил запуск потока

--- src/com/company/Habitat.java
+++ src/com/company/Habitat.java
@@ -23,49 +23,56 @@ public class Habitat {
     static private double P2 = 0.1;
     static GenerateThread thread1;
     static GenerateThread thread2;
+    static RunningAntThread runningWorkerAntThread;
+    static RunningAntThread runningWarriorAntThread;
     private JPanel BottomPanel;
 
-    Habitat(MyPanel window){
+    Habitat(MyPanel window) {
         this.window = window;
 
-   }
+    }
 
     public static void start() {
         try {
             WorkerAnt.worker_ant = ImageIO.read(new File("src/com/company/Picture/SpawnWorcker/B (16).png"));
-        } catch (IOException e) { }
+        } catch (IOException e) {
+        }
         try {
-            WarriorAnt.warrior_ant =ImageIO.read(new File("src/com/company/Picture/SpawnWarrior/W (16).png"));
-        } catch (IOException e) { }
+            WarriorAnt.warrior_ant = ImageIO.read(new File("src/com/company/Picture/SpawnWarrior/W (16).png"));
+        } catch (IOException e) {
+        }
         thread1 = new GenerateThread(window, P1, N1, WorkerAnt.getStaticName());
-        thread2= new GenerateThread(window, P2, N2, WarriorAnt.getStaticName());
+        thread2 = new GenerateThread(window, P2, N2, WarriorAnt.getStaticName());
+        runningWorkerAntThread = new RunningAntThread(window, WorkerAnt.getStaticName(), 10);
+        runningWarriorAntThread = new RunningAntThread(window, WarriorAnt.getStaticName(), 1);
         thread1.start();
         thread2.start();
+        runningWorkerAntThread.start();
+        runningWarriorAntThread.start();
     }
 
-    public static void stop(){
-        if(thread1 != null) thread1.stopThread();
-        if(thread2 != null) thread2.stopThread();
-        for(int i = 0; i < AntExample.list.size(); i++){
-            if(AntExample.list.get(i).getName() == "Warrior Ant")
+    public static void stop() {
+        if (thread1 != null) thread1.stopThread();
+        if (thread2 != null) thread2.stopThread();
+        if (runningWarriorAntThread != null) runningWarriorAntThread.stopThread();
+        if (runningWorkerAntThread != null) runningWorkerAntThread.stopThread();
+        for (int i = 0; i < AntExample.list.size(); i++) {
+            if (AntExample.list.get(i).getName() == "Warrior Ant")
                 WarriorAnt.quantity_ant--;
-            else{
+            else {
                 WorkerAnt.quantity_ant--;
             }
-
         }
         AntExample.list.clear();
     }
 
-
-
-    public void respawn(TypeAnt typeAnt){
-        if(AntExample.list.size()!=0) {
+    public void respawn(TypeAnt typeAnt) {
+        if (AntExample.list.size() != 0) {
             boolean OBJFound = false;
             int i = 0;
             if (typeAnt == TypeAnt.Warrior) {//удалает первый элемент переданного типа и отрисовывает все остальные объекты и всех вспомагательных данных
                 while (i < AntExample.list.size()) {
-                    if(AntExample.list.get(i).getName() != WarriorAnt.getStaticName()){
+                    if (AntExample.list.get(i).getName() != WarriorAnt.getStaticName()) {
                         OBJFound = true;
                         break;
                     }
@@ -74,7 +81,7 @@ public class Habitat {
                 WarriorAnt.quantity_ant--;
             } else {
                 while (i < AntExample.list.size()) {
-                    if(AntExample.list.get(i).getName() != WorkerAnt.getStaticName()){
+                    if (AntExample.list.get(i).getName() != WorkerAnt.getStaticName()) {
                         OBJFound = true;
                         break;
                     }
@@ -82,7 +89,7 @@ public class Habitat {
                 }
                 WorkerAnt.quantity_ant--;
             }
-            if(OBJFound == true) {
+            if (OBJFound == true) {
                 AntExample.idList.remove(AntExample.list.get(i).getId());
                 AntExample.BornList.remove(AntExample.list.get(i).getId());
                 AntExample.list.remove(i);
@@ -91,9 +98,9 @@ public class Habitat {
         }
     }
 
-    public void ChangeProperties(double P1, int N1, double P2, int N2){
-        if(N1 == 0)N1 = 1000;
-        if(N2 == 0)N2 = 1000;
+    public void ChangeProperties(double P1, int N1, double P2, int N2) {
+        if (N1 == 0) N1 = 1000;
+        if (N2 == 0) N2 = 1000;
 
         Habitat.N1 = N1;
         Habitat.P1 = P1;
