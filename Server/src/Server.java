71cb1867011ed4941bc0b1bb2665065481c04b6c
Lanik <Lanik142@gmail.com>
2021-05-13 07:23:10 +0700

    Сервер + клиент, выполнена 2-я часть ANTP-12

---
+++ Server/src/Server.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Socket> clients = new ArrayList<>();
+
+        try{
+            ServerSocket servSocket = new ServerSocket(4000);
+            System.out.println("Started!");
+
+            while (!servSocket.isClosed()) {
+                Socket client = servSocket.accept();
+                System.out.println("Connect! " + client.getInetAddress().getHostAddress() + ' ' + clients.size());
+
+                clients.add(client);
+
+                ClientHandler thread = new ClientHandler(clients);
+                thread.start();
+            }
+        } catch(Exception e){ e.printStackTrace(); }
+    }
+}
